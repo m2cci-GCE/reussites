d2039ef7428caa640979efba5fcf9cdc9c9cb93d
m2cci-GCE <guillaume.communie@gmail.com>
2017-11-14 12:28:40 +0100

    reussite fonctionnelle, debut de code

--- C4.c
+++ C4.c
@@ -1,4 +1,4 @@
-LigneC4 : tableau de 4 tas, un pour chaque couleur
+/*LigneC4 : tableau de 4 tas, un pour chaque couleur
 
 TasCourrant : entier, indice du tas en cours d'utilisation. a e.i. =0 (trefle)
 
@@ -17,7 +17,7 @@ decouvre les cartes caches
 si elle sont sous le bon tas : gagne!
 sinon perdu
 
-
+*/
 
 
 
@@ -26,52 +26,98 @@ sinon perdu
 
 
 #include <stdio.h>
-#include <C4.h>
+#include "C4.h"
+/*Tas de départ*/
 
-// Tas de départ
 Tas LigneTalonC4[DerniereCouleur+1];
 Tas TalonC4;
 Tas LigneC4[DerniereCouleur+1];
 int TasCourant = PremiereCouleur;
 
-// Localisation des 4 tas de départ
+/* Localisation des 4 tas de départ*/
 Localisation LocLigneTalonC4[DerniereCouleur+1];
 Localisation LocTalonC4;
 Localisation LocLigneC4[DerniereCouleur+1];
 
 
-// Placement initial des tas
+/* Placement initial des tas*/
 void SaisirLocTasC4()
 {
 	LocTalonC4.NC = 1;
 	LocTalonC4.NL = 1;
 
-	for (int i=PremiereCouleur ; i<=DerniereCouleur ; i++)
+	int i;
+	for (i=PremiereCouleur ; i<=DerniereCouleur ; i++)
 	{
 		LocLigneTalonC4[i].NC = 2+2*i;
 		LocLigneTalonC4[i].NL = 1;
 
 		LocLigneC4[i].NC = 2+2*i;
-		LocLigneC4[i].NL = 2;
+		LocLigneC4[i].NL = 3;
 	}
 }
 
 void CreerJeuInitialC4()
 {
+	SaisirLocTasC4();
 	CreerJeuNeuf(32,LocTalonC4,&TalonC4);
 	BattreTas(&TalonC4);
 
-	for (int i=PremiereCouleur ; i<=DerniereCouleur ; i++)
+	int i;
+	for (i=PremiereCouleur ; i<=DerniereCouleur ; i++)
 	{
-		// Creer les 4 tas de 8 cartes et les remplis
+		/* Creer les 4 tas de 8 cartes et les remplis*/
 		CreerTasVide(LocLigneTalonC4[i],empile,&LigneTalonC4[i]);
-		for (int j=0 ; j<8 ; j++)
+		int j;
+		for (j=0 ; j<8 ; j++)
 		{
 			DeplacerHautSur(&TalonC4,&LigneTalonC4[i]);
 		}
 
-		// Creer les 4 tas vides pour le tirage
+		/* Creer les 4 tas vides pour le tirage*/
 		CreerTasVide(LocLigneC4[i],etale,&LigneC4[i]);
 	}
 }
 
+void AfficherC4()
+{
+	EffacerGraphique();
+	AfficherTas(LigneTalonC4[1],"Trefle");
+	AfficherTas(LigneTalonC4[2],"Carreau");
+	AfficherTas(LigneTalonC4[3],"Coeur");
+	AfficherTas(LigneTalonC4[4],"Pique");
+
+	int i;
+	for (i=PremiereCouleur ; i<=DerniereCouleur ; i++)
+	{
+		AfficherTas(LigneC4[i],"");
+	}
+	AttendreCliquer();
+}
+
+void JouerUneC4()
+{
+	Couleur Co,CoSource,CoDestination;
+
+	CreerJeuInitialC4();
+	for (Co=PremiereCouleur ; Co<=DerniereCouleur ; Co++)
+	{
+		RetournerCarteSur(&LigneTalonC4[Co]);
+	}
+
+	AfficherC4();
+
+	CoSource = 1;
+	do
+	{
+		CoDestination = LaCouleur(LigneTalonC4[CoSource].queue->elt);
+		DeplacerHautSur(&LigneTalonC4[CoSource],&LigneC4[CoDestination]);
+		if (!TasVide(LigneTalonC4[CoSource]))
+		{
+			RetournerCarteSur(&LigneTalonC4[CoSource]);
+		}
+		CoSource = CoDestination;
+		AfficherC4();
+	}
+	while (!TasVide(LigneTalonC4[CoSource]));
+}
